831d76c0459f4ff1ac5830fa58a034c4c51b59d9
Martín Calderón <martincalderon92@outlook.com>
2021-11-24 14:58:34 -0300

    🔧 Add ESLint config file.

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 13,
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		// 'no-console': ['error', { allow: ['warn', 'error'] }],
+		'react/prop-types': 'off',
+		quotes: ['error', 'single', { allowTemplateLiterals: true }],
+		semi: ['error', 'always'],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+};
