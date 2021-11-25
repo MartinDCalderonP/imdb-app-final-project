e7834129dc10f3765e4d8bae968ef37388dbc24b
Martín Calderón <martincalderon92@outlook.com>
2021-11-24 21:37:21 -0300

    :art: Create Home page and import in App file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
+import Home from './pages/Home';
 
 export default function App() {
-	return <div className="App"></div>;
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<Navigate replace to="/" />} />
+			</Routes>
+		</Router>
+	);
 }
