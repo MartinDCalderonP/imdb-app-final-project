6a8334320e1a58cf0cf7b9f3d9c9a4542d695bf7
Martín Calderón <martincalderon92@outlook.com>
2021-11-24 21:37:01 -0300

    :art: Create Layout component.

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../styles/Layout.module.scss';
+import { ILayoutProps } from '../common/interfaces';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+export default function Layout({ children, footer = true }: ILayoutProps) {
+	return (
+		<>
+			<Navbar />
+			<main className={styles.mainContainer}>{children}</main>
+			{footer && <Footer />}
+		</>
+	);
+}
