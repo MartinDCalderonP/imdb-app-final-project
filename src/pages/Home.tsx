fe8a3140cb236b861c2b960fcce0716a79b8dbf8
Martín Calderón <martincalderon92@outlook.com>
2021-11-25 19:58:57 -0300

    :sparkles: Add section title.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import { IMoviesData, ITvShowsData } from '../common/Interfaces';
 import Layout from '../components/Layout';
 import Spinner from '../components/Spinner';
 import CardsContainer from '../components/CardsContainer';
+import Divider from '../components/Divider';
 
 export default function Home() {
 	const [currentMoviesPage, setCurrentMoviesPage] = useState(1);
@@ -22,19 +23,31 @@ export default function Home() {
 				{movies.loading && tvShows.loading && <Spinner />}
 
 				{!movies.loading && movies?.data && (
-					<CardsContainer
-						loading={movies?.loading}
-						posts={movies?.data?.results}
-						type="movies"
-					/>
+					<>
+						<h1 className={styles.sectionTitle}>
+							{filter.toUpperCase()} MOVIES
+						</h1>
+
+						<CardsContainer
+							loading={movies?.loading}
+							posts={movies?.data?.results}
+							type="movies"
+						/>
+					</>
 				)}
 
 				{!tvShows.loading && tvShows?.data && (
-					<CardsContainer
-						loading={tvShows?.loading}
-						posts={tvShows?.data?.results}
-						type="tvShows"
-					/>
+					<>
+						<h1 className={styles.sectionTitle}>
+							{filter.toUpperCase()} TV SHOWS
+						</h1>
+
+						<CardsContainer
+							loading={tvShows?.loading}
+							posts={tvShows?.data?.results}
+							type="tvShows"
+						/>
+					</>
 				)}
 			</div>
 		</Layout>
