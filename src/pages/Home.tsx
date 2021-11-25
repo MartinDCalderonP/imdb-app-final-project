5c01193492f7492aad338a2b4334728c52eeb137
Martín Calderón <martincalderon92@outlook.com>
2021-11-24 23:20:00 -0300

    :art: Add useFetch and styles to Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from '../styles/Home.module.scss';
 import Layout from '../components/Layout';
+import useFetch from '../hooks/useFetch';
+import { API } from '../common/Enums';
+import { IPopularMovies, IPopularTvShows } from '../common/Interfaces';
 
 export default function Home() {
+	const [currentMoviesPage, setCurrentMoviesPage] = useState(1);
+	const [currentTvShowsPage, setCurrentTvShowsPage] = useState(1);
+	const popularMoviesFetchUrl = `${API.base}${API.popularMovies}?page=${currentMoviesPage}`;
+	const popularTvShowsFetchUrl = `${API.base}${API.popularTvShows}?page=${currentTvShowsPage}`;
+	// const movies = useFetch<IPopularMovies>(popularMoviesFetchUrl);
+	// const tvShows = useFetch<IPopularTvShows>(popularTvShowsFetchUrl);
+
 	return (
 		<Layout>
-			<h1>Home</h1>
+			<div className={styles.home}>XX</div>
 		</Layout>
 	);
 }
