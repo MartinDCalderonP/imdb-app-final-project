72336539c1ba677b6b8320760732472afc7d073c
Martín Calderón <martincalderon92@outlook.com>
2021-11-25 23:43:43 -0300

    :recycle: Change API enum.

--- src/common/Helpers.tsx
+++ src/common/Helpers.tsx
@@ -9,14 +9,13 @@ export const capitalizeWord = (word: string) => {
 
 export const sectionFetchUrl = (
 	currentPage: number,
-	filter: string,
 	type: string
 ): string => {
 	const paginationParams = `?page=${currentPage}`;
 
 	const fetchUrls: IObjects = {
-		movies: `${API.base}${API.movies}${filter}${paginationParams}`,
-		tvShows: `${API.base}${API.tvShows}${filter}${paginationParams}`,
+		movies: `${API.base}${API.movies}${paginationParams}`,
+		tvShows: `${API.base}${API.tvShows}${paginationParams}`,
 	};
 
 	return fetchUrls[type];
