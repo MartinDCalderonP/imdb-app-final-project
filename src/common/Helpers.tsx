79ec899b4dc9e54f8df1b8b9a3b8febe097bd42b
Martín Calderón <martincalderon92@outlook.com>
2021-11-28 17:04:44 -0300

    :sparkles: Add Filters Fetch Url and some change of names.

--- src/common/Helpers.tsx
+++ src/common/Helpers.tsx
@@ -1,27 +1,24 @@
 import { Paths, API } from './Enums';
 import { IObjects } from './Interfaces';
-import { PossiblePost } from './Types';
+import { PossibleSectionPost } from './Types';
 
 export const capitalizeWord = (word: string) => {
 	if (!word) return word;
 	return word[0].toUpperCase() + word.substr(1).toLowerCase();
 };
 
-export const sectionFetchUrl = (
-	currentPage: number,
-	type: string
-): string => {
+export const sectionFetchUrl = (currentPage: number, type: string): string => {
 	const paginationParams = `?page=${currentPage}`;
 
 	const fetchUrls: IObjects = {
-		movies: `${API.base}${API.movies}${paginationParams}`,
-		tvShows: `${API.base}${API.tvShows}${paginationParams}`,
+		movies: `${API.base}${API.popularMovies}${paginationParams}`,
+		tvShows: `${API.base}${API.popularTvShows}${paginationParams}`,
 	};
 
 	return fetchUrls[type];
 };
 
-export const cardsContainerNames = (post: PossiblePost): string => {
+export const cardsContainerNames = (post: PossibleSectionPost): string => {
 	return 'title' in post
 		? post.title
 		: 'name' in post
@@ -40,4 +37,20 @@ export const cardNavigationUrl = (type: string, id: number): string => {
 
 export const cardImageUrl = (image: string): string => {
 	return `${API.images}${API.imageWidth200}${image}`;
+};
+
+export const filtersFetchUrl = (category: string, type: string): string => {
+	const moviesFiltersFetchUrls: IObjects = {
+		certification: `${API.base}${API.moviesCertifications}?`,
+		genre: `${API.base}${API.moviesGenres}?`,
+	};
+
+	const tvShowsFiltersFetchUrls: IObjects = {
+		certification: `${API.base}${API.tvShowsCertifications}?`,
+		genre: `${API.base}${API.tvShowsGenres}?`,
+	};
+
+	return type === 'movies'
+		? moviesFiltersFetchUrls[category]
+		: tvShowsFiltersFetchUrls[category];
 };
