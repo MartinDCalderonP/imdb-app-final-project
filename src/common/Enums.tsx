b199d7b9cd86f72c610e2a8d5d76208d802a2332
Martín Calderón <martincalderon92@outlook.com>
2021-11-28 17:04:16 -0300

    :sparkles: Add API endpoints.

--- src/common/Enums.tsx
+++ src/common/Enums.tsx
@@ -6,8 +6,12 @@ export enum Paths {
 
 export enum API {
 	base = 'https://api.themoviedb.org/3/',
-	movies = 'movie/popular',
-	tvShows = 'tv/popular',
+	popularMovies = 'movie/popular',
+	popularTvShows = 'tv/popular',
+	moviesCertifications = 'certification/movie/list',
+	tvShowsCertifications = 'certification/tv/list',
+	moviesGenres = 'genre/movie/list',
+	tvShowsGenres = 'genre/tv/list',
 	images = 'https://image.tmdb.org/t/p/',
 	imageWidth200 = 'w200',
 }
