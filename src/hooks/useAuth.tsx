10f768499d902d2c6c324934837e13a2eac186ba
Martín Calderón <martincalderon92@outlook.com>
2021-12-03 18:55:23 -0300

    :fire: Delete unused Loading on useAuth.

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -7,7 +7,6 @@ export default function useAuth(type: string, fetchParam?: string) {
 	const [requestToken, setRequestToken] = useState<string>();
 	const [sessionId, setSessionId] = useState<string>();
 	const [sessionDeleted, setSessionDeleted] = useState<string>();
-	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<string>();
 
 	const APIparams = `?api_key=${API_KEY}`;
@@ -36,32 +35,26 @@ export default function useAuth(type: string, fetchParam?: string) {
 
 	useEffect(() => {
 		const fetchData = async () => {
-			setLoading(true);
-
 			fetch(fetchUrls[type], headers)
 				.then((res) => res.json())
 				.then((result) => {
 					if (type === 'requestToken') {
 						setRequestToken(result.request_token);
-						setLoading(false);
 					} else if (type === 'getSessionId') {
 						setSessionId(result.session_id);
-						setLoading(false);
 					} else if (type === 'deleteSession') {
 						setSessionDeleted(result.success);
 						setSessionId('');
-						setLoading(false);
 					}
 				})
 				.catch((err) => {
 					if (err.name === 'AbortError') return;
 					setError(`${err}. Try again later.`);
-					setLoading(false);
 				});
 		};
 
 		fetchData();
 	}, [type]);
 
-	return { requestToken, sessionId, sessionDeleted, loading, error };
+	return { requestToken, sessionId, sessionDeleted, error };
 }
