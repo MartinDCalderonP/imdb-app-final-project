087d7e3706c3ba50cbdbf4d1596d9254f7e0106f
Martín Calderón <martincalderon92@outlook.com>
2021-12-05 12:48:25 -0300

    :sparkles: Create PaginationButtons component.

---
+++ src/components/PaginationButtons.tsx
@@ -0,0 +1,38 @@
+import React, { useMemo } from 'react';
+import styles from '../styles/PaginationButtons.module.scss';
+import { IPaginationButtons } from '../common/Interfaces';
+
+export default function PaginationButtons({
+	totalPosts,
+	postsPerPage,
+	paginate,
+	currentPage,
+}: IPaginationButtons) {
+	const pagesNumbers = useMemo(() => {
+		return Array.from(
+			{ length: Math.ceil(totalPosts / postsPerPage) },
+			(_, i) => 1 + i
+		);
+	}, [totalPosts, postsPerPage]);
+
+	const handlePageButtonClick = (pageNumber: number) => {
+		paginate(pageNumber);
+	};
+
+	return (
+		<div className={styles.buttonsContainer}>
+			{pagesNumbers?.map((pageNumber: number) => (
+				<button
+					key={`paginationButton${pageNumber}`}
+					className={
+						styles.pageButton +
+						(currentPage === pageNumber ? ` ${styles.active}` : '')
+					}
+					onClick={() => handlePageButtonClick(pageNumber)}
+				>
+					{pageNumber}
+				</button>
+			))}
+		</div>
+	);
+}
