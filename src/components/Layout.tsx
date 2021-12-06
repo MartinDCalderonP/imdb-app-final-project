d9c5ea4e5be5cdadb6537adcc0d20b45be443417
Martín Calderón <martincalderon92@outlook.com>
2021-12-06 13:33:37 -0300

    📱 Adjust SearchInput.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styles from '../styles/Layout.module.scss';
 import { ILayoutProps } from '../common/Interfaces';
 import Navbar from './Navbar';
+import SearchInput from './SearchInput';
 import Footer from './Footer';
 
 export default function Layout({ children, footer = true }: ILayoutProps) {
 	return (
 		<>
 			<Navbar />
+			<div className={styles.searchContainer}>
+				<SearchInput />
+			</div>
 			<main className={styles.mainContainer}>{children}</main>
 			{footer && <Footer />}
 		</>
