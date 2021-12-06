64c6271193b95a1387abcb708070841d46753564
Martín Calderón <martincalderon92@outlook.com>
2021-12-06 18:19:01 -0300

    ✅ Create Search Input test.

--- src/__tests__/SearchInput.test.tsx
+++ src/__tests__/SearchInput.test.tsx
@@ -2,10 +2,15 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-// test('user should navigate to Search page', async () => {
-// 	const { findByText } = render(<App />);
-// 	userEvent.type(screen.getAllByPlaceholderText('Search')[0], 'Spider');
-// 	userEvent.click(screen.getAllByRole('button')[0]);
-// 	const spider = await findByText(/Results/i);
-// 	expect(spider).toBeInTheDocument();
-// });
+test('should render ', () => {
+	render(<App />);
+	expect(screen.getAllByPlaceholderText(/search/i)[0]).toBeInTheDocument();
+});
+
+test('user should navigate to Search page', async () => {
+	const { findByText } = render(<App />);
+	userEvent.type(screen.getAllByPlaceholderText('Search')[0], 'Spider');
+	userEvent.click(screen.getAllByRole('button')[0]);
+	const spider = await findByText(/Results/i);
+	expect(spider).toBeInTheDocument();
+});
