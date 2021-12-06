58a18fba4c3f0bcb39c2c7246a3a27a6e8eec31f
Martín Calderón <martincalderon92@outlook.com>
2021-12-06 16:58:04 -0300

    ✅ Create Footer test.

---
+++ src/__tests__/Footer.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import Footer from '../components/Footer';
+
+test('should be rendered', () => {
+	const { getByText } = render(<Footer />);
+	const textElement = getByText(/All rights reserved/i);
+	expect(textElement).toBeInTheDocument();
+});
