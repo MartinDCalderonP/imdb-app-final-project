885628e7bb9fb7416a3157dbb9161710e1673878
Martín Calderón <martincalderon92@outlook.com>
2021-12-05 23:32:56 -0300

    :recycle: Refactor Private Route to use Context.

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Navigate } from 'react-router';
-import useLocalStorage from '../hooks/useLocalStorage';
-import { initialState } from '../context/Reducer';
+import { useContextState } from '../context/Context';
 import { IPrivateRouteProps } from '../common/Interfaces';
 
 export default function PrivateRoute({
 	children,
 	redirectTo,
 }: IPrivateRouteProps) {
-	const [profile, setProfile] = useLocalStorage(
-		'profile',
-		initialState.profile
-	);
+	const { state } = useContextState();
 
-	const isAuthenticated = profile.id === 0 ? false : true;
+	const isAuthenticated = state.profile.id === 0 ? false : true;
 
 	return isAuthenticated ? children : <Navigate to={redirectTo} />;
 }
